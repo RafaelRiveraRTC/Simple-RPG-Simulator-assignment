9b1d193d526f8b7cda5613eb827147b1b18e4ef4
RafaelRiveraRTC <Tropicalcone25@gmail.com>
2024-05-03 11:47:33 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Simple_RPG_Simulator_assignment
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
